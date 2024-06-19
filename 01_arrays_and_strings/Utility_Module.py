6263f10fc03d6f36a4a70b48e2439a16099d1012
Shah <shahdilawar@gmail.com>
2024-06-19 14:07:00 -0700

    Added comments

--- 01_arrays_and_strings/Utility_Module.py
+++ 01_arrays_and_strings/Utility_Module.py
@@ -6,8 +6,10 @@ def utility_decorator(func):
     def wrapper(*args, **kwargs):
         log.info("-" * 60)
         log.info(f"Started {func.__name__} method")
+        # store the inner function valuecd 
         value = func(*args, **kwargs)
         log.info(f"Ended {func.__name__} message")
+        # return the inner func value 
         log.info("-" * 60)
         return value
     return wrapper
